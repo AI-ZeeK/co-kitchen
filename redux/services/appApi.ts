6087e5cedd253e7b1dd6f358d4d8ac98d701cf8a
Isaac Williams <Isaaciyaye174@gmail.com>
2023-09-06 12:39:29 +0100

    Update appApi.ts

--- redux/services/appApi.ts
+++ redux/services/appApi.ts
@@ -1,14 +1,11 @@
 import axios from "axios";
-const API = axios.create({
-  baseURL: "http://3wdz.c.time4vps.cloud:3000",
-});
-const API_URL = "/";
+
 export const getDataApi = async () => {
-  const {data} = await API.get(API_URL);
+  const {data} = await axios.get("http://3wdz.c.time4vps.cloud:3000/");
 
   return data;
 };
 export const getFolderDataApi = async (id: string) => {
-  const res = await API.get(`http://3wdz.c.time4vps.cloud:3000/file/${id}`);
+  const res = await axios.get(`http://3wdz.c.time4vps.cloud:3000/file/${id}`);
   return res.data;
 };
